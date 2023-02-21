c3b045d570d90f0d827e5df9327f553f915439d0
vagrant <vagrant@homestead>
2023-02-21 21:40:28 +0000

    asd

--- scripts/deploy_laravel.sh
+++ scripts/deploy_laravel.sh
@@ -10,30 +10,30 @@ mkdir -p /var/www/html/storage/framework/views
 mkdir -p /var/www/html/storage/framework/cache
 mkdir -p /var/www/html/public/files/
 
-# Install dependencies
-export COMPOSER_ALLOW_SUPERUSER=1
-composer install -d /var/www/html/
+# # Install dependencies
+# export COMPOSER_ALLOW_SUPERUSER=1
+# composer install -d /var/www/html/
 
-# Copy configuration from /var/www/.env, see README.MD for more information
-cp /var/www/.env /var/www/html/.env
+# # Copy configuration from /var/www/.env, see README.MD for more information
+# cp /var/www/.env /var/www/html/.env
 
-# Migrate all tables
-php /var/www/html/artisan migrate
+# # Migrate all tables
+# php /var/www/html/artisan migrate
 
-# Clear any previous cached views
-php /var/www/html/artisan config:clear
-php /var/www/html/artisan cache:clear
-php /var/www/html/artisan view:clear
+# # Clear any previous cached views
+# php /var/www/html/artisan config:clear
+# php /var/www/html/artisan cache:clear
+# php /var/www/html/artisan view:clear
 
-# Optimize the application
-php /var/www/html/artisan config:cache
-php /var/www/html/artisan optimize
-#php /var/www/html/artisan route:cache
+# # Optimize the application
+# php /var/www/html/artisan config:cache
+# php /var/www/html/artisan optimize
+# #php /var/www/html/artisan route:cache
 
-# Change rights
-chmod 777 -R /var/www/html/bootstrap/cache
-chmod 777 -R /var/www/html/storage
-chmod 777 -R /var/www/html/public/files/
+# # Change rights
+# chmod 777 -R /var/www/html/bootstrap/cache
+# chmod 777 -R /var/www/html/storage
+# chmod 777 -R /var/www/html/public/files/
 
-# Bring up application
-php /var/www/html/artisan up
+# # Bring up application
+# php /var/www/html/artisan up
