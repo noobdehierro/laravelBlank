1fb726be3b123f2ab985a62f5b4611875cfa0e2c
vagrant <vagrant@homestead>
2023-02-21 21:02:23 +0000

    prueba otra

--- scripts/deploy_laravel.sh
+++ scripts/deploy_laravel.sh
@@ -1,39 +1,39 @@
-# #!/bin/bash
+!/bin/bash
 
-# # Enter html directory
-# cd /var/www/html/
+# Enter html directory
+cd htdocs
 
-# # Create cache and chmod folders
-# mkdir -p /var/www/html/bootstrap/cache
-# mkdir -p /var/www/html/storage/framework/sessions
-# mkdir -p /var/www/html/storage/framework/views
-# mkdir -p /var/www/html/storage/framework/cache
-# mkdir -p /var/www/html/public/files/
+# Create cache and chmod folders
+mkdir -p htdocs/bootstrap/cache
+mkdir -p htdocs/storage/framework/sessions
+mkdir -p htdocs/storage/framework/views
+mkdir -p htdocs/storage/framework/cache
+mkdir -p htdocs/public/files/
 
-# # Install dependencies
-# export COMPOSER_ALLOW_SUPERUSER=1
-# composer install -d /var/www/html/
+# Install dependencies
+export COMPOSER_ALLOW_SUPERUSER=1
+composer install -d htdocs/
 
-# # Copy configuration from /var/www/.env, see README.MD for more information
-# cp /var/www/.env /var/www/html/.env
+# Copy configuration from /var/www/.env, see README.MD for more information
+cp /var/www/.env htdocs/.env
 
-# # Migrate all tables
-# php /var/www/html/artisan migrate
+# Migrate all tables
+php htdocs/artisan migrate
 
-# # Clear any previous cached views
-# php /var/www/html/artisan config:clear
-# php /var/www/html/artisan cache:clear
-# php /var/www/html/artisan view:clear
+# Clear any previous cached views
+php htdocs/artisan config:clear
+php htdocs/artisan cache:clear
+php htdocs/artisan view:clear
 
-# # Optimize the application
-# php /var/www/html/artisan config:cache
-# php /var/www/html/artisan optimize
-# #php /var/www/html/artisan route:cache
+# Optimize the application
+php htdocs/artisan config:cache
+php htdocs/artisan optimize
+#php htdocs/artisan route:cache
 
-# # Change rights
-# chmod 777 -R /var/www/html/bootstrap/cache
-# chmod 777 -R /var/www/html/storage
-# chmod 777 -R /var/www/html/public/files/
+# Change rights
+chmod 777 -R htdocs/bootstrap/cache
+chmod 777 -R htdocs/storage
+chmod 777 -R htdocs/public/files/
 
-# # Bring up application
-# php /var/www/html/artisan up
+# Bring up application
+php htdocs/artisan up
