640b63d00e920ab276ae1ca3b7cf687131934557
vagrant <vagrant@homestead>
2023-03-16 19:14:57 +0000

    instalacion

--- scripts/deploy_laravel.sh
+++ scripts/deploy_laravel.sh
@@ -39,10 +39,10 @@
 # php /var/www/html/artisan up
 cd /var/www/MyProject/
 sudo composer install
-# sudo cp .env.example .env
+sudo cp .env.example .env
 sudo chmod 777 -R storage/*
 sudo php artisan key:generate
-sudo php artisan migrate
+# sudo php artisan migrate
 #nada
 # mkdir -p /var/www/html/public/files/
 # mkdir -p /var/www/html/public/files/
