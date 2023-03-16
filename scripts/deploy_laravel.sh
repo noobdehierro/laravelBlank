de90df91098713e74ea81c1dfd9cbbb4882b2bde
vagrant <vagrant@homestead>
2023-03-16 20:11:47 +0000

    env

--- scripts/deploy_laravel.sh
+++ scripts/deploy_laravel.sh
@@ -37,11 +37,11 @@
 
 # # Bring up application
 # php /var/www/html/artisan up
-# cd /home/ubuntu/figoupos/
-# sudo composer install
-# # sudo cp .env.example .env
-# sudo chmod 777 -R storage/*
-# # sudo php artisan key:generate
+cd /home/ubuntu/laravelblank/
+sudo composer install
+sudo cp .env.example .env
+sudo chmod 777 -R storage/*
+sudo php artisan key:generate
 # sudo php artisan migrate --seed
 #nada
 # mkdir -p /var/www/html/public/files/
